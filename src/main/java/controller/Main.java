a2b83e2b39ce3a8b226fe5ec2064fd3830058488
Mariano Maisonnave <mariano.maisonnave@cs.uns.edu.ar>
2025-04-19 14:45:33 -0300

    Changed title of main window (from Hermosilla to Kloster)

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -189,7 +189,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("Maisonnave Hermosilla - Estudio Contable ");
+        this.primaryStage.setTitle("Maisonnave Kloster - Estudio Contable ");
         this.primaryStage.setMaximized(true);
         this.primaryStage.getIcons().add(new Image("file:Resources/Images/iconito.png"));
       
